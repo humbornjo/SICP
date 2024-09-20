f399f2371edda50f5d9ea4c8d92bcab8bdabca51
humbornjo <humbornjo@gmail.com>
2024-09-20 20:33:54 +0800

    update: nomo weekend sync

---
+++ prac/stream.scm
@@ -0,0 +1,107 @@
+(define true #t)
+(define false #f)
+(define nil '())
+
+(define (prime? x) 
+  (define (test divisor) 
+    (cond ((> (* divisor divisor) x) true) 
+          ((= 0 (remainder x divisor)) false) 
+          (else (test (+ divisor 1))))) 
+  (test 2))
+
+(define (cons-stream a b) 
+  (cons a (delay b)))
+
+(define stream-null? null?)
+(define the-empty-stream nil)
+
+(define (stream-ref s n)
+  (if (= n 0)
+    (stream-car s)
+    (stream-ref (stream-cdr s) (- n 1))))
+(define (stream-map proc s)
+  (if (stream-null? s)
+    the-empty-stream
+    (cons-stream (proc (stream-car s))
+                 (stream-map proc (stream-cdr s)))))
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+    'done
+    (begin (proc (stream-car s))
+           (stream-for-each proc (stream-cdr s)))))
+
+(define (display-line x) (newline) (display x))
+(define (display-stream s)
+  (stream-for-each display-line s))
+(define (display-line x) (newline) (display x))
+
+
+(define (stream-car stream) (car stream))
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+    the-empty-stream
+    (cons-stream
+      low
+      (stream-enumerate-interval (+ low 1) high))))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter
+                        pred
+                        (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+(define (stream-filterprime? stream)
+  (stream-filter prime? stream))
+
+(display "the prime after 10007 is: ")
+(display (stream-car
+  (stream-cdr
+    (stream-filterprime?
+                   (stream-enumerate-interval
+                     10000 10013)))))
+(newline)
+
+(define delay (lambda (proc) (memo-proc (lambda () proc))))
+(define (force delayed-object) (delayed-object))
+
+(define (memo-proc proc)
+  (let ((already-run? false) (result false))
+    (lambda ()
+      (if (not already-run?)
+        (begin (set! result (proc))
+               (set! already-run? true)
+               result)
+        result))))
+
+; ex 3.50
+(newline)
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+    the-empty-stream
+    (cons-stream
+      (apply proc (map stream-car argstreams))
+      (apply stream-map
+             (cons proc (map stream-cdr argstreams))))))
+
+; ex 3.51
+(define (stream-map proc s)
+  (if (stream-null? s)
+    the-empty-stream
+    (cons-stream (proc (stream-car s))
+                 (stream-map proc (stream-cdr s)))))
+
+(define (show x)
+  (display-line x)
+  x)
+
+(define x
+  (stream-map show
+              (stream-enumerate-interval 0 1)))
+;; (stream-ref x 5)
+;; (stream-ref x 7)
+
