e3561ff4aff72e9b54c5f9fc483913544fa26087
humbornjo <humbornjo@gmail.com>
2024-10-30 01:18:56 +0800

    update: ex 4.18
    .

--- prac/metacircular.scm
+++ prac/metacircular.scm
@@ -695,4 +695,22 @@
 ;; spawned. and the easiest way to achieve that is merge the inner 
 ;; lambda and outter lambda
 
+; ex 4.18
 
+;; to determine if it works, we have to figure out how <let> works
+;; let -> lambda -> procedure
+
+;; in let -> lambda, what actually happends is (eval ((lambda) vals))
+;; and the vals will be evaluated one by one, so it won't work.
+
+; ex 4.19
+
+;; why it is an issue? when define is performed on a function, it 
+;; will finally be converted to a binding like (var, procedure) in 
+;; the given env, nothing else. which means it is lazy eval.
+
+;; however, if it is performed on a parameter, then the parameter
+;; will be eval immidiately. which will cause dependency error. 
+
+;; I would throw a error, and the simultaneous define will be devised
+;; in my impl of intepretor
