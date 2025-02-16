e9b180389f35ed0dc6c25fa41fd8d08e11f3d66f
humbornjo <humbornjo@gmail.com>
2025-02-17 02:06:02 +0800

    chore: make it easy to test
    fix: ex4_16

---
+++ prac/ch04/ex4_22.scm
@@ -0,0 +1,16 @@
+;; main
+
+(load "init_util.scm")
+(load "eval_separate.scm")
+
+(define (let->combination exp)
+  (let ((clauses (cadr exp))
+        (body (caddr exp)))
+    (let ((vars (map car clauses))
+          (vals (map cadr clauses)))
+      (cons (make-lambda vars body) vals))))
+
+(define (analyze-let exp)
+  (analyze (let->combination exp)))
+
+(assert (eq? 20 ((analyze '(let ((x 2) (y 10)) (* x y))) the-global-environment)))
---
+++ prac/ch04/repl.scm
@@ -0,0 +1,4 @@
+(load "init_util.scm")
+(load "eval_metacircular.scm")
+
+(driver-loop)
